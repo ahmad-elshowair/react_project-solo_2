c6cdab57debf3b1431f427d1090a8f26eca2c4b5
Ahmad Elshowair <it.is.work@outlook.com>
2022-03-24 12:26:12 +0700

    style navbar component

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -3,8 +3,8 @@ import glob_icon from '../images/glob-icon.png';
 export const Navbar = ()=>{
     return(
         <nav className="navbar">
-            <img src={glob_icon} alt="global icon"/>
-            <h3>my travel journal.</h3>
+            <img src={glob_icon} alt="global icon" className='nav__icon'/>
+            <h3 className='nav_title'>my travel journal.</h3>
         </nav>
     )
 };
